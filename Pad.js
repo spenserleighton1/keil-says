ac88586bf10ce11b66de9ea6284bb5f1a337cb9e
Spenser Leighton <spenser.leighton@gmail.com>
2018-11-08 10:26:19 -0700

    Create Pad component

---
+++ Pad.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button, Alert} from 'react-native';
+
+class Pad extends Component {
+  constructor() {
+    super()
+  }
+
+  
+}
